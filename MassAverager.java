6297497119cce56a9e9daeb522afcc810420e486
Laurambarly <41225960+Laurambarly@users.noreply.github.com>
2021-09-12 11:17:30 -0700

    Update MassAverager.java

--- MassAverager.java
+++ MassAverager.java
@@ -4,7 +4,7 @@ public class MassAverager
 {
 	Planet[] thePlanets = Planet.getAll();
 	
-	// Complete this. Retrieve the array of planets, then compute average mass.
+	// Retrieve the array of planets, then compute average mass.
 	public float getMeanPlanetaryMass()
 	{
 		float total = 0;
@@ -24,16 +24,6 @@ public class MassAverager
 	
 	
 	
-	
-	//
-	// In almost all classes in almost all 46B homework assignments, the main()
-	// method is for you to test your code. The autograder doesn't look at the
-	// output from main().
-	//
-	// Since this assignment is simple, there's really only 1 useful version of
-	// main(), and it's provided here. Later, when your assignments are more
-	// complicated, your main() will change several or many times as you develop
-	// different pieces of your assignment.
 	public static void main(String[] args)
 	{
 		MassAverager averager = new MassAverager();
